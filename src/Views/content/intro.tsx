e8665bb4bd7ddf3812acb3f7191170105279b23e
Jose De Los Reyes <jddelosreyes23@gmail.com>
2022-12-13 19:27:49 -0500

    common html files content converted into .tsx

---
+++ src/Views/content/intro.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+function Intro() {
+    return (
+        <div className="container" id="mainContent">
+            <h1 id="MainTitle" className="col-lg-offset-1 col-lg-10">The Travel Report</h1>
+            <h4 className="col-lg-offset-1 col-lg-10">body copy and image credits: Sophie-Claire Hoeller</h4>
+            <article id="9Islands" className="row">
+                <h2 className="col-md-offset-1 col-md-10 col-xs-offset-1 col-xs-10">9 Gorgeous Greek islands</h2>
+                <p id="IntroParagraph" className="col-md-4 col-xs-offset-1 col-xs-10">
+                    Now is the time to travel to Greece. The country may be going through an economic crisis, but many travellers say that it hasn't impacted the experience of visiting. Plus, there may be some great deals.
+                    Greece has 1,400 islands, though only 230 of them are inhabited.
+                    And while everyone knows about Santorini and Mykonos, there are gorgeous lesser-known islands in Greece, too.
+                </p>
+                <img className="col-md-6 col-xs-offset-1  col-xs-10" src="../../Assets/images/milos-greece-colorful-houses.jpg" alt="Milos Greece colorful houses" />
+            </article>
+            <hr />
+
+            <section className="row">
+                <p className="col-md-12 col-xs-offset-1 col-xs-10">Here are 9 incredible Greek islands you may not have heard of, but should have on your radar.</p>
+                Now is the time to travel to Greece. The country may be going through an economic crisis, but many travellers say that it hasn't impacted the experience of visiting. Plus, there may be some great deals.
+                Greece has 1,400 islands, though only 230 of them are inhabited.
+                And while everyone knows about Santorini and Mykonos, there are gorgeous lesser-known islands in Greece, too.
+            </section>
+        </div>
+    );
+}
+
---
+++ src/Views/content/syros.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+function Syros() {
+    return (
+        <div className="container">
+            <article id="Syros" className="row">
+                <h2 className="col-md-10 col-xs-offset-1 col-xs-10">Syros</h2>
+                <img className="col-md-10 col-xs-offset-1 col-xs-10" src="../../Assets/images/syros.jpg" alt="Syros" />
+                <p className="col-md-10 col-xs-offset-1 col-xs-10" id="Syros">
+                    A favourite Athenian escape and former shipowners' vacation spot full of grand mansions, Syros is little known to foreigners. Famous for its neoclassical architecture and perfectly preserved heritage, its capital, Ermoupolis, was a 13th-century Venetian-built town and important trade and industrial centre, and boasts giant churches and beautiful villas, a stunning town hall, and labyrinthine streets and stairways.
+                </p>
+            </article>
+            <hr />
+        </div>
+    )
+}
+
